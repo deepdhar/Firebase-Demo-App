9e7dfc595118a0c138a87bc5fd132118acb8d21c
deepdhar <dhar2017.slg@gmail.com>
2020-05-17 02:20:32 +0530

    Nutrifrnd App with added authentication, and added navigation to Explore App button.

--- project_app_flutter/lib/main.dart
+++ project_app_flutter/lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        home: MainScreen(),
+        theme: ThemeData.dark());
   }
 }
 
--- project_app_flutter/lib/mainscreen.dart
+++ project_app_flutter/lib/mainscreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'secondary_mainscreen.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
 
@@ -26,10 +28,9 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[200],
       appBar: AppBar(
         title: Center(child: Text('Nutrifrnd')),
-        backgroundColor: Colors.red[300],
+        backgroundColor: Colors.green[400],
       ),
       body: SafeArea(child: _buildBody()),
     );
@@ -37,33 +38,7 @@ class _MainScreenState extends State<MainScreen> {
 
   Widget _buildBody() {
     if (_currentUser != null) {
-      return Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: _currentUser,
-            ),
-            title: Text(
-              'Hello, ',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            subtitle: Text(
-              _currentUser.displayName ?? '',
-              style: TextStyle(
-                fontSize: 17.0,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-          RaisedButton(
-            onPressed: _handleSignOut,
-            child: Text('SIGN OUT'),
-          )
-        ],
-      );
+      return _signin();
     } //Secondary Main Page
     else {
       return Column(
@@ -102,7 +77,7 @@ class _MainScreenState extends State<MainScreen> {
             child: FlatButton(
               onPressed: _handleSignIn,
               child: Card(
-                color: Colors.red,
+                color: Colors.red[600],
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
                 child: ListTile(
                   leading: Padding(
@@ -132,6 +107,93 @@ class _MainScreenState extends State<MainScreen> {
     } //SignIn Page
   }
 
+  Widget _signin() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          color: Colors.black38,
+          margin: EdgeInsets.only(top: 0.0),
+          child: ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: _currentUser,
+            ),
+            title: Text(
+              'Hello, ',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              _currentUser.displayName ?? '',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          child: RaisedButton(
+            color: Colors.green[500],
+            onPressed: _handleSignOut,
+            child: Text('SIGN OUT'),
+          ),
+        ), //SignOut Button
+        SizedBox(height: 80.0),
+        Container(
+          child: FlatButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondaryScreen()),
+              );
+            },
+            child: Card(
+              margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              color: Colors.pink[600],
+              child: ListTile(
+                title: Center(
+                  child: Text(
+                    'Explore the App',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Basic',
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ), //Explore Button
+        Container(
+          child: FlatButton(
+            onPressed: _launchURL,
+            child: Card(
+              margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              color: Colors.pink[600],
+              child: ListTile(
+                title: Center(
+                  child: Text(
+                    'Get Your Diet Plan',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontFamily: 'Basic',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
   Future<void> _handleSignIn() async {
     try {
       await _googleSignIn.signIn();
@@ -142,5 +204,14 @@ class _MainScreenState extends State<MainScreen> {
 
   Future<void> _handleSignOut() async {
     _googleSignIn.disconnect();
+  }
+
+  _launchURL() async {
+    const url = 'https://bit.ly/nutrifrnds';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
