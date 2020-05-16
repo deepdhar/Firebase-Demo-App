3dafb1486715027d71a2a77fe448202399ad5598
deepdhar <dhar2017.slg@gmail.com>
2020-05-16 22:14:22 +0530

    Nutrifrnd App with added authentication.

--- project_app_flutter/lib/mainscreen.dart
+++ project_app_flutter/lib/mainscreen.dart
@@ -1,91 +1,146 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class MainScreen extends StatelessWidget {
+GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  GoogleSignInAccount _currentUser;
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    _googleSignIn.signInSilently();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurpleAccent,
+      backgroundColor: Colors.pink[200],
       appBar: AppBar(
         title: Center(child: Text('Nutrifrnd')),
-        backgroundColor: Colors.deepPurple[700],
+        backgroundColor: Colors.red[300],
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                  );
-                },
-                child: Card(
-                  color: Colors.green[400],
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                  child: ListTile(
-                    title: Center(
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontFamily: 'Rubik',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
+      body: SafeArea(child: _buildBody()),
+    );
+  }
+
+  Widget _buildBody() {
+    if (_currentUser != null) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: _currentUser,
+            ),
+            title: Text(
+              'Hello, ',
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+            subtitle: Text(
+              _currentUser.displayName ?? '',
+              style: TextStyle(
+                fontSize: 17.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: _handleSignOut,
+            child: Text('SIGN OUT'),
+          )
+        ],
+      );
+    } //Secondary Main Page
+    else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 50.0),
+          CircleAvatar(
+            backgroundImage: AssetImage('images/icon.jpg'),
+            radius: 60.0,
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Welcome to",
+                style: TextStyle(
+                  fontFamily: 'Rubik',
+                  fontSize: 20.0,
+                ),
+              ),
+              Text(
+                " Nutrifrnd",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 150.0,
+          ),
+          Container(
+            child: FlatButton(
+              onPressed: _handleSignIn,
+              child: Card(
+                color: Colors.red,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+                child: ListTile(
+                  leading: Padding(
+                    padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: Icon(
+                      FontAwesomeIcons.google,
                     ),
                   ),
-                ),
-              ),
-            ), //Log In
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 5.0),
-              child: Text(
-                'OR',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-            ), //Or
-            Container(
-              child: FlatButton(
-                onPressed: () {},
-                child: Card(
-                  color: Colors.red,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                  child: ListTile(
-                    leading: Padding(
-                      padding: const EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
-                      child: Icon(
-                        FontAwesomeIcons.google,
-                      ),
-                    ),
-                    title: Padding(
-                      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-                      child: Text(
-                        'Sign-In with Google',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontFamily: 'SourceSansPro',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                  title: Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                    child: Text(
+                      'Sign-In with Google',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ],
+      );
+    } //SignIn Page
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> _handleSignOut() async {
+    _googleSignIn.disconnect();
   }
 }
